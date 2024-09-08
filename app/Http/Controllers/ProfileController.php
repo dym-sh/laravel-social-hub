609bc98701fc9297fad140b4acce6bc9c10834fd
Dym Sohin <you@example.com>
2024-09-08 10:07:01 +0200

    cover-upload

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Http\RedirectResponse;
@@ -19,7 +20,7 @@ class ProfileController extends Controller
         return Inertia::render('Profile/View', [
             'mustVerifyEmail' => $user instanceof MustVerifyEmail,
             'status' => session('status'),
-            'user' => $user,
+            'user' => new UserResource( $user ),
         ]);
     }
 
@@ -58,5 +59,31 @@ class ProfileController extends Controller
         $request->session()->regenerateToken();
 
         return Redirect::to('/');
+    }
+
+    public function updateImage( Request $request )
+    {
+        $user = $request->user();
+
+        $data = $request->validate([
+            'cover' => [ 'nullable', 'image'],
+            'avatar' => [ 'nullable', 'image'],
+        ]);
+
+        $avatar = $data['avatar'] ?? null;
+        /**
+         * @var \Illuminate\Http\UploadedFile $cover
+         */
+        $cover = $data['cover'] ?? null;
+
+        if($cover) {
+            $path = $cover->store( 'avatars/'.$user->id, 'public' );
+
+            $user->update(['cover_path' => $path]);
+
+            session('success', 'cover image updated');
+
+            return back()->with('status', 'cover-image-updated');
+        }
     }
 }
